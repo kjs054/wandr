93dfb68b6423d1d79859c96bc55249281dba2b2a
Wandr Inc <kjs054111@gmail.com>
2019-07-10 18:10:17 -0400

    Adjust font size for width information view

--- Wandr/Views/InformationView.swift
+++ Wandr/Views/InformationView.swift
@@ -18,6 +18,7 @@ class InformationView: UIView {
     let informationTitle: UILabel = {
         let label = UILabel()
         label.font = UIFont(name: "NexaBold", size: 20)
+        label.adjustsFontSizeToFitWidth = true
         label.textColor = wandrBlue
         return label
     }()
@@ -25,6 +26,7 @@ class InformationView: UIView {
     let informationSubTitle: UILabel = {
         let label = UILabel()
         label.font = UIFont(name: "Avenir-Heavy", size: 17)
+        label.adjustsFontSizeToFitWidth = true
         label.textColor = #colorLiteral(red: 0.5333333333, green: 0.5333333333, blue: 0.5333333333, alpha: 1)
         label.numberOfLines = 0
         label.textAlignment = .center
