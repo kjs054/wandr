d7ad145bd513fd2265702def75e56342cb85a045
Wandr Inc <kjs054111@gmail.com>
2019-08-04 23:51:52 -0400

    init messages with dictionary and then init messages variables with dictionary value

--- Wandr/Model/Plans/message.swift
+++ Wandr/Model/Plans/message.swift
@@ -7,12 +7,23 @@
 //
 
 import Foundation
+import Firebase
+import FirebaseFirestore
 
 struct Message {
     var sender: User
-    var timestamp: Date
+    var timestamp: Timestamp
     var content: String
     var type: Type
+    var isFromCurrentLoggedInUser: Bool
+    
+    init(dictionary: [String: Any]) {
+        self.sender = dictionary["sender"] as! User
+        self.timestamp = dictionary["timestamp"] as? Timestamp ?? Timestamp(date: Date())
+        self.content = dictionary["content"] as? String ?? ""
+        self.type = getType(type: dictionary["type"] as! String)
+        self.isFromCurrentLoggedInUser = Auth.auth().currentUser?.uid == self.sender.uid
+    }
 }
 
 enum Type {
@@ -22,4 +33,13 @@ enum Type {
     case openTable
     case facebook
     case uber
+}
+
+func getType(type: String) -> Type {
+    switch type {
+    case "text":
+        return .text
+    default:
+        return .text
+    }
 }
